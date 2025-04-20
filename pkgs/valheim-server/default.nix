2e1ee5b1dac697ee66cf00897e87b0f0e2d73126
Andreas K. <hamburger1984@gmail.com>
2025-04-20 22:26:52 +0200

    Updating to manifest 18007466826975597

--- pkgs/valheim-server/default.nix
+++ pkgs/valheim-server/default.nix
@@ -10,8 +10,8 @@ stdenv.mkDerivation (finalAttrs: {
     inherit (finalAttrs) name;
     appId = "896660";
     depotId = "896661";
-    manifestId = "6248571692813894409";
-    hash = "sha256-dz2mpv1pVku4XkOio5la/QOM7nTw72LiKQuQSffB+Bc=";
+    manifestId = "18007466826975597";
+    hash = "sha256-8UdoLzKiu8CEztqwTHGP5M3RdrrVUTmAwN6Cqt9R+v8=";
   };
 
   # Skip phases that don't apply to prebuilt binaries.
