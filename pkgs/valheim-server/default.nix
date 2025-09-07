09be8317363f3e18bb24e4268f9f2b070a1acee4
Andreas K. <hamburger1984@gmail.com>
2025-09-07 12:48:15 +0200

    Back to the old hash?!

--- pkgs/valheim-server/default.nix
+++ pkgs/valheim-server/default.nix
@@ -11,8 +11,8 @@ stdenv.mkDerivation (finalAttrs: {
     appId = "896660";
     depotId = "896661";
     manifestId = "18007466826975597";
-    #hash = "sha256-8UdoLzKiu8CEztqwTHGP5M3RdrrVUTmAwN6Cqt9R+v8=";
-    hash = lib.fakeHash;
+    hash = "sha256-8UdoLzKiu8CEztqwTHGP5M3RdrrVUTmAwN6Cqt9R+v8=";
+    #hash = lib.fakeHash;
     branch = "public-test";
     branchPassword = "yesimadebackups";
     debug = true;
